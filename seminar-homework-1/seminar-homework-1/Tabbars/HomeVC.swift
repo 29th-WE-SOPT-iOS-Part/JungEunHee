a39b6aff0303127c0d7b8bf8ebd5fbd7fdcfcfe1
EunHee-Jeong <18102007@seoultech.ac.kr>
2021-10-28 14:07:09 +0900

    💄[MOD] : 탭바 화면 storyboard -> VC 수정, 폴더 옮김

---
+++ seminar-homework-1/seminar-homework-1/Tabbars/HomeVC.swift
@@ -0,0 +1,18 @@
+//
+//  HomeVC.swift
+//  seminar-homework-1
+//
+//  Created by 정은희 on 2021/10/28.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
