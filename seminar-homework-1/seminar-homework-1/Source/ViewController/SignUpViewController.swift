2463efc93d0d54f4e391d67b572aedb8d9ec5161
EunHee-Jeong <18102007@seoultech.ac.kr>
2021-11-14 21:24:37 +0900

    ✅[CHORE] : SignUpVC 코드 수정 (#13)

--- seminar-homework-1/seminar-homework-1/Source/ViewController/SignUpViewController.swift
+++ seminar-homework-1/seminar-homework-1/Source/ViewController/SignUpViewController.swift
@@ -44,23 +44,12 @@ class SignUpViewController: UIViewController {
     }
     
     // alert 창을 띄우기 위한 함수
-    func showAlert(title: String, message: String) {
-        let alert = UIAlertController(title: title,
-                                      message: message,
-                                      preferredStyle: .alert)
-        
-        let okAction = UIAlertAction(title: "확인", style: .default) {_ in
-            if message == "회원 가입 성공" {
-                guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as? ResultViewController else {return}
-                
-                nextVC.message = self.nameTextField.text
-                self.present(nextVC, animated: true, completion: nil)
-            }
+    private func showAlert(_ message: String, completion: ((UIAlertAction) -> Void)?) {
+            let alert = UIAlertController(title: "회원가입", message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "확인", style: .default, handler: completion)
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
         }
-        
-        alert.addAction(okAction)
-        present(alert, animated: true)
-    }
     
     
     // MARK: - IBAction Part
@@ -96,23 +85,27 @@ extension SignUpViewController {
                                      email: contactTextField.text ?? "",
                                      password: pwTextField.text ?? "") { responseData in
             switch responseData {
-            case .success(let signupResponse):
-                guard let response = signupResponse as? SignupResponseData else {return}
-                if response.data != nil {
-                    self.showAlert(title: "회원가입",
-                                     message: "회원 가입 성공")
+            case .success(let loginResponse):
+                guard let response = loginResponse as? LoginResponseData else {return}
+                self.showAlert(response.message) { _ in
+                    guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as? ResultViewController else {return}
+                    
                     UserDefaults.standard.set(self.nameTextField.text, forKey: "message")
+                    
+                    self.present(nextVC, animated: true, completion: nil)
                 }
-            case .requestErr:
-                print("requestErr")
-                self.showAlert(title: "회원가입",
-                                 message: "필요한 값이 없습니다.")
+                
+            case .requestErr(let msg):
+                guard let message = msg as? String else {return}
+                self.showAlert(message, completion: nil)
+                
             case .pathErr:
                 print("pathErr")
-                self.showAlert(title: "회원가입",
-                                 message: "이미 사용 중인 이메일입니다.")
-            case .serverErr:
-                print("serverErr")
+                
+            case .serverErr(let msg):
+                guard let message = msg as? String else {return}
+                self.showAlert(message, completion: nil)
+
             case .networkFail:
                 print("networkFail")
             }
