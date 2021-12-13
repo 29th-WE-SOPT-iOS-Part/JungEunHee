9814d930c9a70a5081806ae402f9163a86c4bbbd
EunHee-Jeong <18102007@seoultech.ac.kr>
2021-12-13 22:05:55 +0900

    🔨[FIX] : 4차 도전과제 오류 해결과 주석 제거 (#15)

--- seminar-homework-1/seminar-homework-1/Source/NetworkModel/SignupResponseDataModel.swift
+++ seminar-homework-1/seminar-homework-1/Source/NetworkModel/SignupResponseDataModel.swift
@@ -7,12 +7,8 @@
 
 import Foundation
 
-// 회원가입 request에 대한 response를 담을 파일
-// (서버 통신 결과로 받는 응답 body를 디코딩할 데이터 모델)
-
 // MARK: - SignUpResponseData
 struct SignupResponseData: Codable {
-    // Codable -> 데이터를 JSON 데이터 포맷으로 자유롭게 encoding, decoding 할 수 있도록 해주는 프로토콜
     
     let status: Int
     let success: Bool
@@ -23,6 +19,5 @@ struct SignupResponseData: Codable {
 // MARK: - SignUpResultData
 struct SignupResultData: Codable {
     let id: Int
-    // let password: Int
     let name, email: String
 }
